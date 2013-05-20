1757f1736e6ae421dfaced27c1aecc5f6226c589
Stefan Pettersson <stefan@plan3.se>
2013-05-20 22:50:59 +0200

    Memory submission

--- interactivepython/Memory/memory.py
+++ interactivepython/Memory/memory.py
@@ -2,43 +2,91 @@
 # https://class.coursera.org/interactivepython-002/human_grading/view/courses/970391/assessments/32/submissions
 
 import simplegui
+import math
 import random
 
-WIDTH = 800
-HEIGHT = 100
+"""
+note: as long as number of cards is set to an even
+square (16, 25, 36, etc) all should look nice
+"""
+WIDTH = 400
+HEIGHT = 400
 NUM_CARDS = 16
-CARD_WIDTH = WIDTH / NUM_CARDS
+SIDE = math.sqrt(NUM_CARDS)
+CARD_WIDTH = WIDTH / SIDE
+CARD_HEIGHT = HEIGHT / SIDE
 
+# game state constants
+STATE_START = 0
+STATE_SINGLE_CARD = 1
+STATE_END_OF_TURN = 2
+
+# globals
 cards = range(0, NUM_CARDS / 2) + range(0, NUM_CARDS / 2)
 exposed = []
+state = STATE_START
+cards_in_play = [-1, -1]
+turns = 0
 
-# helper function to initialize globals
 def init():
     global exposed
     random.shuffle(cards)
     exposed = [False] * len(cards)
-    exposed[3] = True
+    turns = 0
+    label.set_text("Moves = " + str(turns))
+    cards_in_play = [-1, -1]
 
      
-# define event handlers
 def mouseclick(pos):
-    global exposed
-    card = pos[0] // CARD_WIDTH
+    global exposed, state, cards_in_play, turns
+
+    # calculate what card was clicked
+    card = (pos[1] // CARD_HEIGHT) * SIDE + pos[0] // CARD_WIDTH
+    
     if not exposed[card]:
         exposed[card] = True
-    
-                        
-# cards are logically 50x100 pixels in size    
-def draw(c):
-    for i in range(0, len(cards)):
-        if exposed[i]:
-            c.draw_text(str(cards[i]), (15 + i * CARD_WIDTH, 65), 42, "White")
+        
+        if state == STATE_START:
+            state = STATE_SINGLE_CARD
+            cards_in_play[0] = card
+        elif state == STATE_SINGLE_CARD:
+            state = STATE_END_OF_TURN
+            cards_in_play[1] = card
         else:
-            c.draw_polygon([(i * CARD_WIDTH, 0), (i * CARD_WIDTH + CARD_WIDTH, 0), (i * CARD_WIDTH + CARD_WIDTH, 100), (i * CARD_WIDTH, 100), (i * CARD_WIDTH, 0)], 1, "White", "Green")
+            state = STATE_SINGLE_CARD
+            
+            # hide the two exposed cards if not the same
+            if cards[cards_in_play[0]] != cards[cards_in_play[1]]:
+                exposed[cards_in_play[0]] = False
+                exposed[cards_in_play[1]] = False            
+
+            cards_in_play = [card, -1]
+
+        # count turns
+        if state == STATE_SINGLE_CARD:
+            turns += 1
+            label.set_text("Moves = " + str(turns))
+
+            
+def draw(c):    
+    # draw cards
+    for i in range(0, len(cards)):
+        # calculate x, y position of card 
+        x = i % SIDE * CARD_WIDTH
+        y = i // SIDE * CARD_HEIGHT
+        
+        # if card currently in play, draw blue background
+        if i in cards_in_play:
+            c.draw_polygon([(x, y), (x + CARD_WIDTH, y), (x + CARD_WIDTH, y + CARD_HEIGHT), (x, y + CARD_HEIGHT), (x, y)], 1, "Black", "Blue")
+
+        if exposed[i]:
+            c.draw_text(str(cards[i]), (x - 10 + CARD_WIDTH // 2, y + 10 + CARD_HEIGHT // 2), 42, "White")
+        else:
+            c.draw_polygon([(x, y), (x + CARD_WIDTH, y), (x + CARD_WIDTH, y + CARD_HEIGHT), (x, y + CARD_HEIGHT), (x, y)], 1, "White", "Green")
 
 
 # create frame and add a button and labels
-frame = simplegui.create_frame("Memory", 800, 100)
+frame = simplegui.create_frame("Memory", WIDTH, HEIGHT)
 frame.add_button("Restart", init)
 label = frame.add_label("Moves = 0")
 
@@ -51,6 +99,3 @@ frame.set_draw_handler(draw)
 
 # get things rolling
 frame.start()
-
-
-# Always remember to review the grading rubric
